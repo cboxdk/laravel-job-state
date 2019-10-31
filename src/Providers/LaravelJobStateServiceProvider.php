da67c229e0e27f958bba11fe8c1c0915473ad249
Sylvester Nielsen <sln@easyinspect.io>
2019-10-31 15:06:00 +0100

    Initial Commit

---
+++ src/Providers/LaravelJobStateServiceProvider.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Cbox\LaravelJobStatus\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelJobStateServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->loadMigrationsFrom(__DIR__ . '/migrations');
+        $this->mergeConfigFrom(__DIR__ . '/../config/job-status.php', 'job-status');
+        $this->publishes([
+            __DIR__ . '../database/migrations/' => database_path('migrations'),
+        ], 'migrations');
+        $this->publishes([
+            __DIR__ . '/../config/' => config_path(),
+        ], 'config');
+        //$this->bootListeners();
+    }
+
+    private function bootListeners()
+    {
+        /** @var EventManager $eventManager */
+        $eventManager = app(config('job-status.event_manager'));
+        // Add Event listeners
+        app(QueueManager::class)->before(function (JobProcessing $event) use ($eventManager) {
+            $eventManager->before($event);
+        });
+        app(QueueManager::class)->after(function (JobProcessed $event) use ($eventManager) {
+            $eventManager->after($event);
+        });
+        app(QueueManager::class)->failing(function (JobFailed $event) use ($eventManager) {
+            $eventManager->failing($event);
+        });
+        app(QueueManager::class)->exceptionOccurred(function (JobExceptionOccurred $event) use ($eventManager) {
+            $eventManager->exceptionOccurred($event);
+        });
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
